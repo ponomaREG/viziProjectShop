55e4efa9acb234cdc3a02aa52e7a49cd6334435a
ponomaREG <ponomaREG>
2020-09-08 13:44:12 +0300

    init

--- config.py
+++ config.py
@@ -5,3 +5,5 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class BaseConfig(object):
     DATABASES_FOLDER = os.path.join(basedir,'databases')
     DB_SHOP = os.path.join(DATABASES_FOLDER,'shop.db')
+    
+    JSON_AS_ASCII = False
