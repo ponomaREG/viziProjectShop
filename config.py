075879d3712681e20cf0396d26ac7835ce1e30a7
Disf <ponomaREG>
2020-09-08 13:41:00 +0300

    init

---
+++ config.py
@@ -0,0 +1,7 @@
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+class BaseConfig(object):
+    DATABASES_FOLDER = os.path.join(basedir,'databases')
+    DB_SHOP = os.path.join(DATABASES_FOLDER,'shop.db')
